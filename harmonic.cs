9e5f96736694db09a4b1b0946d0bc9f45937ac11
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-08 09:30:25 +0530

    Harmonic no

---
+++ harmonic.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloWorld
+{
+    public class harmonicno
+    {
+        public static void Harmonic()
+        {
+
+            double result = 0.0, num = 0;
+            int n,j;
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            // INPUT DATA
+            Console.Write("\n\n");
+            Console.Write("enter the harmonic you want to find: ");
+            
+
+            // COMPUTATION
+            for (j = 1; j <= n; j++)
+            {
+                num = num + (1 / j);
+                result = num;
+            }
+            Console.WriteLine("the value of harmonic no. " + n + " is: " + result);
+
+        }
+
+    }
+}
+    
